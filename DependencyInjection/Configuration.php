a2f25c0340a321986a51a50d81b4ac39e2b6b1ff
olmounir <omar.lmounir@capgemini.com>
2021-09-14 10:44:33 +0100

    TreeBuilder compatibility with symfony 4.4

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $treeBuilder->root('liuggio_excel');
+        $treeBuilder = new TreeBuilder('liuggio_excel');
+        //$treeBuilder->root('liuggio_excel');
 
         return $treeBuilder;
     }
